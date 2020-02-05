49e75fdc6580ebe575b3a47d16325326cf37fd1b
JeanneBertrand <jebertra@etud.insa-toulouse.fr>
2020-02-06 00:56:01 +0100

    updated javadoc

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"controller"},{"l":"database"},{"l":"model"},{"l":"view"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"view"}]
